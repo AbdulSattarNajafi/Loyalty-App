796bc60deb1f7492d9ce22e00b67c8d0fa42c7bf
AbdulSattarNajafi <66928318+AbdulSattarNajafi@users.noreply.github.com>
2021-01-29 18:35:50 +0700

    genral settings btns changed

--- components/general_settings/settings/subcmp_scripts.js
+++ components/general_settings/settings/subcmp_scripts.js
@@ -1,1 +1,12 @@
-
+//Function for Inactive buttons
+function inactiveBtns() {
+  let inactiveBTns = document.querySelectorAll('.inactive_btn');
+  inactiveBTns.forEach(activeBtn => {
+    activeBtn.addEventListener('click', (e) => {
+      activeBtn.classList.remove('btn-seconday')
+      activeBtn.classList.toggle('btn-success');
+      e.target.textContent == 'Active' ? e.target.textContent = 'Inactive' : e.target.textContent = 'Active';
+    })
+  });
+};
+inactiveBtns();
--- components/tally/today/subcmp_scripts.js
+++ components/tally/today/subcmp_scripts.js
@@ -6,3 +6,33 @@ $('#today_nav button').unbind('click').click(function(e) {
   $(this).addClass('btn-primary'); 
   $(this).removeClass('btn-light'); 
 });
+
+//function For Flag and Hand Buttons
+function flagActive(){
+  let flagBtns = document.querySelectorAll('.flag_btn');
+  let handBtns = document.querySelectorAll('.hand_btn');
+  flagBtns.forEach(flagbtn => {
+    flagbtn.addEventListener('click', (e) => {
+      let currentRow = e.currentTarget.parentElement.parentElement;
+      if(currentRow.classList.contains('red')) {
+        currentRow.classList.remove('red')
+        currentRow.classList.add('yellow')
+      }else{
+        currentRow.classList.toggle('yellow');
+      }
+    })
+  });
+  //Hand Btns
+  handBtns.forEach(handBtn => {
+    handBtn.addEventListener('click', (e) => {
+      let currentRow = e.currentTarget.parentElement.parentElement;
+      if(currentRow.classList.contains('yellow')) {
+        currentRow.classList.remove('yellow');
+        currentRow.classList.add('red');
+      }else{
+        currentRow.classList.toggle('red');
+      }
+    })
+  });
+};
+flagActive();
